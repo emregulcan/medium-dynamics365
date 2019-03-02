c79e85f607bde45c9ed5741a1d03c5cb98767352
Emre GULCAN <emregulcan@gmail.com>
2019-03-02 05:02:39 +0300

    IOrganizationService kullanımı
    https://medium.com/@dynamics365/visual-studio-c-projelerinde-dynamics-365-ce-e-erişim-a676eae40c3f
    yazısı için örnek projeler

---
+++ source/ConnectToDynamics365/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using MediumD365.Helper;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace ConnectToDynamics365
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IOrganizationService organizationService = null;
+
+                //INFO : Alternate_01 metodunu kullanabilmek için kullanıcı adı + parola + Dynamics 365 Online url bilgisi gereklidir
+                organizationService = Alternate_01("KULLANICIADI@ORGANIZASYONADI.onmicrosoft.com", "PAROLA", "https://ORGANIZASYONADI.crm4.dynamics.com");
+
+                //INFO : Alternate_02 metodunu kullanabilmek için app.config dosyasında <connectionStrings> altında tanımlama yapılmalıdır.
+                organizationService = Alternate_02("D365TR_Sandbox");
+
+
+                /*
+                 * INFO : Tüm projelerimizde ortak bir yapı kullanmak istersek "MediumD365.Helper" projesinde yer alan ConnectionHelper class kullanılabilir.
+                 * Alternate_02 ile benzer şekilde Dynamics 365 bağlantı bilgileri config yapısı üzerinde tutulmaktadır.
+                 * app.config 'e direk yazmak yerine Properties>Settings üzerinden ekleme yapılmıştır.
+                 */
+                ConnectionHelper connectionHelper = new ConnectionHelper("D365TR_Sandbox");
+                organizationService = connectionHelper.GetCrmServiceClient();
+                organizationService = connectionHelper.GetIOrganizationService();
+
+
+                if (organizationService != null)
+                {
+                    WhoAmIRequest request = new WhoAmIRequest();
+                    WhoAmIResponse response = (WhoAmIResponse)organizationService.Execute(request);
+
+                    Console.WriteLine($"User ID : {response.UserId}");
+                    Console.WriteLine($"BusinessUnit ID : {response.BusinessUnitId}");
+                    Console.WriteLine($"Organization ID : {response.OrganizationId}");
+                }
+            }
+            catch (FaultException<OrganizationServiceFault> organizationFaultException)
+            {
+                /*
+                 * INFO : 
+                 * Dynamics 365 SDK tarafından oluşan hataları FaultException<OrganizationServiceFault>
+                 * yapısını kullanarak yakalabiliriz.
+                 * 
+                 */
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Kapatmak için bir tuşa basınız");
+            Console.ReadLine();
+        }
+
+
+
+        static IOrganizationService Alternate_01(string username, string password, string organizationServiceUrl)
+        {
+            IOrganizationService result = null;
+
+            ClientCredentials clientCredentials = new ClientCredentials();
+            clientCredentials.UserName.UserName = username;
+            clientCredentials.UserName.Password = password;
+
+            Uri organizationServiceUri = new Uri(organizationServiceUrl);
+
+            OrganizationServiceProxy proxy = new OrganizationServiceProxy(organizationServiceUri, null, clientCredentials, null);
+
+            result = (IOrganizationService)proxy;
+
+            return result;
+        }
+
+        static IOrganizationService Alternate_02(string connectionName)
+        {
+            IOrganizationService result = null;
+
+            var connectionString = ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+
+            if (crmServiceClient.OrganizationServiceProxy != null)
+            {
+                result = (IOrganizationService)crmServiceClient.OrganizationServiceProxy;
+            }
+            else if (crmServiceClient.OrganizationWebProxyClient != null)
+            {
+                result = (IOrganizationService)crmServiceClient.OrganizationWebProxyClient;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ source/MediumD365.Helper/ConnectionHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace MediumD365.Helper
+{
+    public class ConnectionHelper
+    {
+        #region | Private Definitions  |
+
+        readonly string _connectionName = string.Empty;
+
+        #endregion
+
+        #region | Constructors |
+
+        public ConnectionHelper(string connectionName)
+        {
+            _connectionName = connectionName;
+        }
+
+        #endregion
+
+        #region | Public Methods |
+
+        public IOrganizationService GetIOrganizationService(Guid? callerId = null)
+        {
+            IOrganizationService result = null;
+
+            var crmConnection = GetCrmServiceClient(callerId);
+
+            if (crmConnection != null && crmConnection.IsReady)
+            {
+                if (crmConnection.OrganizationServiceProxy != null)
+                {
+                    result = (IOrganizationService)crmConnection.OrganizationServiceProxy;
+                }
+                else if (crmConnection.OrganizationWebProxyClient != null)
+                {
+                    result = (IOrganizationService)crmConnection.OrganizationWebProxyClient;
+                }
+
+            }
+
+            return result;
+        }
+
+        public CrmServiceClient GetCrmServiceClient(Guid? callerId = null)
+        {
+            CrmServiceClient result = null;
+
+            var connectionString = Properties.Settings.Default[_connectionName].ToString();
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                result = new CrmServiceClient(connectionString);
+
+                if (callerId.HasValue && callerId.Value.Equals(Guid.Empty))
+                {
+                    result.CallerId = callerId.Value;
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
